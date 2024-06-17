36129f7c8a71736a20d01aa202fde62cdcbe6e5e
Colin Newton-Harris <col1695@hotmail.com>
2024-06-17 21:31:05 +0100

    Addition game progress

--- Maths Game/Program.cs
+++ Maths Game/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MathsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var timeNow = DateTime.Now;
+
+            Console.WriteLine("Welcome to the Maths Game");
+            Console.WriteLine("What is your name?");
+            var playerName = Console.ReadLine();
+            Console.WriteLine($"Hi {playerName}, it is {timeNow}. Please choose what game you would like to play");
+            Console.WriteLine("A = Addition\nS = Subtraction\nM = Multiplication\nD = Division\nQ = Quit Playing");
+            var pChoice = Console.ReadLine().Trim().ToLower();
+            var keepPlaying = true;
+
+            switch (pChoice)
+            {
+                case "a":
+                    Console.WriteLine("You have chose the Addition game.");
+                    do
+                    {
+                        Random random = new Random();
+                        var firstNumber = random.Next(0, 101);
+                        var secondNumber = random.Next(0,101);
+                        var sumResult = firstNumber + secondNumber;
+                        Console.WriteLine($"What is {a} + {b}?");
+                        var pAnswer = Console.ReadLine();
+                        Convert.ToInt32(pAnswer);
+                        var playAgain = "";
+                        if(pAnswer == sumResult)
+                        {
+                            Console.WriteLine("Correct!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Incorrect, the answer is {c}");
+                        }
+                        Console.WriteLine("Would you like you play again, enter Y or N?");
+                        playAgain = Console.ReadLine().Trim().ToLower();
+                        if(playAgain == "y")
+                        {
+                            keepPlaying = true;
+                        }
+                        else
+                        {
+                            keepPlaying= false;
+                        }
+                    } while (keepPlaying == true);
+                    break;
+
+                case "s":
+                    Console.WriteLine("You have chose the Subtraction game.");
+                    break;
+
+                case "m":
+                    Console.WriteLine("You have chose the Multiplication game.");
+                    break;
+
+                case "d":
+                    Console.WriteLine("You have chose the Division game.");
+                    break;
+
+                case "q":
+                    Console.WriteLine("Thank you for playing the Maths Game!");
+                    break;
+            }
+        }
+    }
+}
